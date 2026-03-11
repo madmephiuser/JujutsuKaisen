755962b877d76ad84c47a789669a0b7de4c6ef73
madmephiuser <m.visloushkina@yandex.ru>
2026-03-11 16:05:00 +0300

    создание

---
+++ src/main/java/com/mycompany/jujutsukaisen/JujutsuKaisen.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.jujutsukaisen;
+
+/**
+ *
+ * @author Professional
+ */
+public class JujutsuKaisen {
+
+    public static void main(String[] args) {
+
+    }
+}
